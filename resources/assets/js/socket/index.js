8eee5bbe254ffa3fe6ae341c686e1556f7a78344
Tran Dinh Trung <trandinhtrung176@gmail.com>
2016-04-25 11:10:06 +0700

    Fix

--- resources/assets/js/socket/index.js
+++ resources/assets/js/socket/index.js
@@ -1,6 +1,6 @@
 import Socket from 'socket.io-client';
 
-var socketUrl = 'http://rabbitnote.dev:3000';
+var socketUrl = 'https://rabbitnote.herokuapp.com:3000';
 var io = new Socket(socketUrl);
 
 export default io;
